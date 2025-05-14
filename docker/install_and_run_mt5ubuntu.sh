36bc49abeae487ccdf9efd68c0fb1cb4c3c3d417
Arnaud <arnaud@nolimitdevelopment.com>
2025-05-14 08:13:01 +0000

    Fixed folder path in build

--- docker/install_and_run_mt5ubuntu.sh
+++ docker/install_and_run_mt5ubuntu.sh
@@ -4,5 +4,6 @@ winecfg -v=win10
 wineboot -i
 
 cd /root/.wine/drive_c/fx/ && wget "https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe"
+#cd /root/.wine/drive_c/fx/ && wget -O mt5setup.exe "https://download.mql5.com/cdn/web/14111/mt5/vantageinternational5setup.exe"
 /usr/bin/wine64 /root/.wine/drive_c/fx/mt5setup.exe /auto
 
