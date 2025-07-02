22602a948af9816324a7770b0ef38d1dd6be745b
Arnaud <arnaud@nolimitdevelopment.com>
2025-07-02 20:17:54 +0000

    mt5 ubuntu focal

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,6 @@
-docker build -f ./docker/Dockerfile.freqai -t bluestart83/mt5-docker:dev .
+#docker build -f ./docker/Dockerfile.freqai -t bluestart83/mt5-docker:dev .
 docker push bluestart83/mt5-docker:dev
+docker push bluestart83/mt5-histo:dev
 
 #docker build -f ./docker/Dockerfile.freqai_rl -t bluestart83/freqtrade:dev_freqaitorch_rl_bitget .
 #docker push bluestart83/freqtrade:dev_freqaitorch_rl_bitget
